25e977fcc5bc6bf58c8644bfd89e4dbbf38aa7d6
abdelhadi saaoud <saaoud.abdelhadi@gmail.com>
2026-08-19 12:13:52 +0100

    fix some controlle essues

--- user-service/src/main/java/user_service/controllers/AuthController.java
+++ user-service/src/main/java/user_service/controllers/AuthController.java
@@ -1,12 +1,15 @@
 package user_service.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import user_service.dto.AuthResponse;
 import user_service.dto.LoginRequest;
 import user_service.dto.RegisterRequest;
+import user_service.dto.UserDto;
 import user_service.services.AuthService;
 
 
@@ -18,12 +21,13 @@ class AuthController{
     final AuthService authService;
 
     @PostMapping("/login")
-    public String loginUser(LoginRequest request, String token){
-        return authService.loginService(request, token).toString();
+    public  ResponseEntity<AuthResponse> loginUser(LoginRequest request, String token){
+        AuthResponse authResponse = authService.loginService(request, token);
+        return ResponseEntity.status(200).body(authResponse);
     }
 
     @PostMapping("/register")
-    public String registerUser(RegisterRequest request){
-        return authService.registerService(request).toString();
+    public ResponseEntity<UserDto> registerUser(RegisterRequest request){
+        return ResponseEntity.ok(authService.registerService(request));
     }
 }
